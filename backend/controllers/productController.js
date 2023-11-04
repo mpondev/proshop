bd631a30dc2469d75eb9d247f4a01383d0b64cbc
Mario Ponce <mario@mpondev.com>
2023-11-04 21:04:08 +0100

    Add User routes and controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,8 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
-// @route   GET /api/products
 // @desc    Fetch all products
+// @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
