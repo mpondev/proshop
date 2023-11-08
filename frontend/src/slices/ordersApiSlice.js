3f82da11d74031c6a179d63dc3221353f5bc38c5
Mario Ponce <mario@mpondev.com>
2023-11-08 16:40:24 +0100

    Add list orders for admin

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -35,13 +35,20 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getOrders: builder.query({
+      query: () => ({
+        url: ORDERS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
 export const {
   useCreateOrderMutation,
-  useGetOrderDetailsQuery,
   useGetMyOrdersQuery,
+  useGetOrderDetailsQuery,
+  useGetOrdersQuery,
   usePayOrderMutation,
   useGetPayPalClientIdQuery,
 } = ordersApiSlice;
