a11507cc19c673f0c1c6b6a60741b916f20190cd
Mario Ponce <mario@mpondev.com>
2023-11-10 19:33:56 +0100

    Paginate Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import { Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import { useGetProductsQuery } from '../slices/productsApiSlice.js';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 function HomeScreen() {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -19,7 +21,7 @@ function HomeScreen() {
         <>
           <h1>Latest products</h1>
           <Row>
-            {products.map(product => (
+            {data.products.map(product => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
